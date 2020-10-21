128cbe3e2dc3634e6e3fee8cdd37be3c9295c244
Tobias Dahl Nielsen <tobiasdahlnielsen@live.dk>
2020-10-21 08:47:11 +0200

    sæson

---
+++ season.R
@@ -0,0 +1,27 @@
+library(readr)
+spot_data <- as.data.frame(read_csv("spot_data.csv"))
+
+
+periode <- 365*24
+
+model <- glm(spot_data[,3]~
+                time(spot_data[,1])+
+                # I(time(spot_data[,1])^2)+
+                cos((2*pi/periode)*I(time(spot_data[,1])))+
+                sin((2*pi/periode)*I(time(spot_data[,1])))+
+                cos((4*pi/periode)*I(time(spot_data[,1])))+
+                sin((4*pi/periode)*I(time(spot_data[,1])))+
+                cos((8*pi/periode)*I(time(spot_data[,1])))+
+                sin((8*pi/periode)*I(time(spot_data[,1])))+
+                cos((24*pi/periode)*I(time(spot_data[,1])))+
+                sin((24*pi/periode)*I(time(spot_data[,1])))+
+                cos((104*pi/periode)*I(time(spot_data[,1])))+
+                sin((104*pi/periode)*I(time(spot_data[,1])))
+                )
+
+
+par(mfrow=c(2,1))
+plot(spot_data[,3],type = "l")
+plot(model$residuals,type="l")
+par(mfrow=c(1,1))
+plot(spot_data$StartUTC,model$fitted.values,type = "l")
