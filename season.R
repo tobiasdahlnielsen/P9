9a7b2332a7fb9a896fca28db5204df89a06e8ab8
tobiasdahlnielsen <43407100+tobiasdahlnielsen@users.noreply.github.com>
2020-11-22 20:28:34 +0100

    armagarch

--- season.R
+++ season.R
@@ -1,7 +1,7 @@
 ## Load packages
 source("Lib.R")
-load("~/P9/armagarchparm.RData")
-
+#load("~/P9/armagarchparm.RData")
+load("~/P9/5arga.RData")
 ## Load data
 spot_data <- as.data.frame(read_csv("spot_data.csv"))
 spot_data <- spot_data %>% mutate(StartUTC = as.POSIXct(StartUTC, tz = "UTC", format = "%Y-%m-%d %H:%M"))
