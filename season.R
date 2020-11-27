0099a4bfd90bf7e738010b15d07952e2135788a5
Tobias Dahl Nielsen <tobiasdahlnielsen@live.dk>
2020-11-27 09:29:09 +0100

    data

--- season.R
+++ season.R
@@ -12,10 +12,20 @@ spot_data <- spot_data %>% mutate(StartUTC = as.POSIXct(StartUTC, tz = "UTC", fo
 
 {
 dd <- seasonaldummy(ts(spot_data$DE,frequency = 24))
+  wh = rep(2,length(spot_data$wd))
+  wh[which(spot_data$wd < 6)] = 1
+  wh
+spot_data <- spot_data %>% mutate(hd = hour(StartUTC)+1, wd = wday(StartUTC), md = day(StartUTC), yd = month(StartUTC))
 
+
+
+spot_data$wd
 data <- spot_data[,1]
 datafit <- spot_data[,1]
 periode <- 365*24
+
+
+spot_data <- spot_data[17543:26278,]
 for (i in 1:4) {
   model <- glm(spot_data[,i+1]~
                  time(spot_data[,1])+
@@ -23,41 +33,92 @@ for (i in 1:4) {
                  I(time(spot_data[,1])^3)+
                  I(time(spot_data[,1])^4)+
                  I(time(spot_data[,1])^5)+
-                 #I(time(spot_data[,1])^6)+
+                 I(time(spot_data[,1])^6)+
                  #I(time(spot_data[,1])^7)+
                  #I(time(spot_data[,1])^8)+
-                 cos((2*pi/periode)*I(time(spot_data[,1])))+
-                 sin((2*pi/periode)*I(time(spot_data[,1])))+
-                 cos(((2*2)*pi/periode)*I(time(spot_data[,1])))+
-                 sin(((2*2)*pi/periode)*I(time(spot_data[,1])))+
-                 cos(((4*2)*pi/periode)*I(time(spot_data[,1])))+
-                 sin(((4*2)*pi/periode)*I(time(spot_data[,1])))+
-                 cos(((12*2)*pi/periode)*I(time(spot_data[,1])))+
-                 sin(((12*2)*pi/periode)*I(time(spot_data[,1])))+
-                 cos(((52*2)*pi/periode)*I(time(spot_data[,1])))+
-                 sin(((52*2)*pi/periode)*I(time(spot_data[,1])))+
-                 cos(((365*2)*pi/periode)*I(time(spot_data[,1])))+
-                 sin(((365*2)*pi/periode)*I(time(spot_data[,1])))+
-                 cos(((2*365*2)*pi/periode)*I(time(spot_data[,1])))+
-                 sin(((2*365*2)*pi/periode)*I(time(spot_data[,1])))+
-                 dd
+                 # cos((2*pi/periode)*I(time(spot_data[,1])))+
+                 # sin((2*pi/periode)*I(time(spot_data[,1])))+
+                 # cos(((2*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # sin(((2*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # cos(((4*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # sin(((4*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # cos(((12*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # sin(((12*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # cos(((52*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # sin(((52*2)*pi/periode)*I(time(spot_data[,1])))+
+                 #  cos(((365*2)*pi/periode)*I(time(spot_data[,1])))+
+                 #  sin(((365*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # cos(((2*365*2)*pi/periode)*I(time(spot_data[,1])))+
+                 # sin(((2*365*2)*pi/periode)*I(time(spot_data[,1])))+
+                 factor(spot_data$hd)+
+                 factor(spot_data$wd)+
+                 factor(spot_data$md)+
+                 factor(spot_data$yd)
   )
- 
-#ARMAtest <- auto.arima(model$residuals)
+summary(model)
+  # plot(spot_data[3600:4400,2],type="l")
+  # lines(model$fitted.values[3600:4400],col="red")
+  # plot(model$residual,type="l")
+acf(model$residuals,lag.max = 100)
+
+model2 <- auto.arima(model$residuals)
+
+plot(model2$residuals[1:1000],type="l")
+lines(model$residuals[1:1000],col="red")
+acf(model2$residuals,lag.max = 300)
+
+sarma2 <- arima(model2$residuals,seasonal = list(order=c(3,0,0),period=24))
+summary(sarma2)
+acf(sarma2$residuals,lag.max = 3000)
+Box.test(sarma2$residuals,type = "Ljung-Box", lag=4)
+plot(sarma2$residuals[1:1000],type="l")
+hist(sarma2$residuals[1:1000],breaks = 80)
+  
+ARMAtest <- auto.arima(ts(model$residuals,frequency = 24))
+summary(ARMAtest)
+acf(ARMAtest$residuals,lag.max = 800)
+plot(ARMAtest$residuals,type="l")
+
+
+sarma <- arima(model$residuals,seasonal = list(order=c(3,0,0),period=24))
+acf(sarma$residuals,lag.max=400)
+
+model3 <- auto.arima(sarma$residuals)
+summary(model3)
+acf(model3$residuals,lag.max=300)
+
+arma <- permutations(6,2,v=0:5,repeats.allowed = TRUE)
+
+for (i in 1:dim(arma)[1]) {
+  vari <- arima(sarma$residuals,order = c(arma[i,1],0,arma[i,2]))$residuals
+  Acf(vari,main=arma[i,],lag.max = 300)
+  pacf(vari,main=arma[i,],lag.max = 300)
+}
+
+
+Box.test(model3$residuals,type="Ljung-Box", lag=48)
+
+
+summary(sarma)
+acf(sarma$residuals,lag.max = 1000)
+
 spec <- ugarchspec(variance.model = list(model = "sGARCH",
-                                         garchOrder = bp[[i]][3:4],
+                                         garchOrder = c(0,1),
                                          submodel = NULL,
                                          external.regressors = NULL,
                                          variance.targeting = FALSE),
 
-                   mean.model     = list(armaOrder =  c(bp[[i]][1:2]),
+                   
+                   mean.model     = list(armaOrder =  c(5,1),
                                          external.regressors = NULL,
                                          distribution.model = "norm",
                                          start.pars = list(),
                                          fixed.pars = list()))
 
-garch <- ugarchfit(spec = spec, data = model$residuals,solver = "hybrid", solver.control = list(trace=0))
-sarma <- arima(garch@fit$residuals,seasonal = list(order=c(5,0,0),period=24))
+
+garch <- ugarchfit(spec = spec, data = sarma$residuals,solver = "hybrid", solver.control = list(trace=0))
+#sarma <- arima(garch@fit$residuals,seasonal = list(order=c(5,0,0),period=24))
+acf(garch@fit$residuals,lag.max = 300)
 
 
 data <- cbind(data,garch@fit$residuals)
